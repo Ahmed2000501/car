2cfc6ffa37cd5386a371d38d1cec9a601514ba93
Ahmed2000501 <91163313+Ahmed2000501@users.noreply.github.com>
2023-01-21 18:45:48 +0200

    Add files via upload

---
+++ js/mian.js
@@ -0,0 +1,9 @@
+let img =document.querySelector('.img');
+let container =document.querySelector('.container');
+function cars(car){
+    img.src = car;
+}
+
+function colars(color){
+    container.style.background =color;
+}
